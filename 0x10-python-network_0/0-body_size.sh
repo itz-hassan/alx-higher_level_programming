e2f94153b6e691415089cac66aee43a032d4968b
Hassan Usman <husmanjah@gmail.com>
2023-08-09 12:30:18 +0100

    test2

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a request and displays the size of the response body
-curl -sI "$1" | sed -En "s/Content-Length: (.*)/\1/p"
+# takes URL and displays the size of the response body
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
